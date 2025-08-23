17b73a61efdf0d199cfcded1aebd5e33983ceb0d
A-Qassem <aqassemm2004@gmail.com>
2025-08-23 16:28:16 +0300

    add RoadMap and Phase Classes

---
+++ DomainLayer/Models/RoadMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class RoadMap
+    {
+        public int RoadMapId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int Total_Months { get; set; }
+        public int Total_Hours { get; set; }
+        public Level Diffculty_Level { get; set; }  
+        public List<Phase> Phases { get; set; } = new List<Phase>();
+        public List<string> Career_Outcomes { get; set; } = new List<string>();
+    }
+}
