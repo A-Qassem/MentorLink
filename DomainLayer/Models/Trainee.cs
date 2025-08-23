2d106b44446bf7af95ac60b2ee453aa292c158f8
BassimEhab <bassimstudy@gmail.com>
2025-08-23 15:27:55 +0300

    Add project files.

---
+++ DomainLayer/Models/Trainee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+   
+    public class Trainee
+    {
+        
+    }
+}
