d6adedb0502d591767079322879363a7e22dd459
BassimEhab <bassimstudy@gmail.com>
2025-08-23 15:29:27 +0300

    Add Models

--- DomainLayer/Models/Mentor.cs
+++ DomainLayer/Models/Mentor.cs
@@ -9,6 +9,19 @@ namespace DomainLayer.Models
 {
     public class Mentor
     {
-        
+        public int MentorId { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Email { get; set; } = null!;
+        public string? PictureUrl { get; set; }
+        public required string Password { get; set; }
+        public required string Phone { get; set; }
+        public string? Description { get; set; }
+        public string? JopTitle { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public List<string> Tags { get; set; } = new List<string>();
+        public List<string> Skills { get; set; } = new List<string>();
     }
 }
--- DomainLayer/Models/Trainee.cs
+++ DomainLayer/Models/Trainee.cs
@@ -8,9 +8,33 @@ using System.Threading.Tasks;
 
 namespace DomainLayer.Models
 {
-   
+    public enum Level
+    {
+        Beginner,
+        Intermediate,
+        Advanced
+    }
     public class Trainee
     {
-        
+        public int TraineeId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        public string? PictureUrl { get; set; }
+
+        public required string Password { get; set; }
+        public required string Phone { get; set; }
+
+        public Level Level { get; set; } = Level.Beginner;
+
+        public bool IsUpdated { get; set; } = false;
+
+        public bool IsSubscribed { get; set; } = false;
+        public List<string> PreferredLanguages { get; set; } = new List<string>();
+        public List<string> FocusAreas { get; set; } = new List<string>();
     }
 }
