4cc5678d4ac3c0b5d048b0fd08e9d17a74135b1f
BassimEhab <bassimstudy@gmail.com>
2025-08-24 17:16:00 +0300

    Create Generic Repository

---
+++ DomainLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+    }
+}
---
+++ Presistence/Repositories/GenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using DomainLayer.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Presistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presistence.Repositories
+{
+    public class GenericRepository<TEntity>(AppDbContext _dbcontext) : IGenericRepository<TEntity> where TEntity : class
+    {
+        public async Task AddAsync(TEntity entity) => await _dbcontext.Set<TEntity>().AddAsync(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbcontext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(int id) => await _dbcontext.Set<TEntity>().FindAsync(id);
+
+        public void Remove(TEntity entity) => _dbcontext.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity) => _dbcontext.Set<TEntity>().Update(entity);
+    }
+}
