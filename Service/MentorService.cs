c81f1990daa16cd7f47fdc99b7328d13dc9cd655
BassimEhab <bassimstudy@gmail.com>
2025-08-27 23:42:10 +0300

    Create mentor service

---
+++ Service/MentorService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DomainLayer.Contracts;
+using DomainLayer.Models;
+using ServiceAbstraction;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class MentorService(IUnitOfWork _unitOfWork, IMapper mapper) : IMentorService
+    {
+        public async Task<IEnumerable<MentorDto>> GetAllMentorsAsync()
+        {
+            var Repo = _unitOfWork.GetRepository<Mentor>();
+            var mentors = await Repo.GetAllAsync();
+            var mentorDtos = mapper.Map<IEnumerable<Mentor>,IEnumerable<MentorDto>>(mentors);
+            return mentorDtos;
+
+        }
+
+        public async Task<MentorDto> GetMentorById(int id)
+        {
+            var Repo = _unitOfWork.GetRepository<Mentor>();
+            var mentor = await Repo.GetByIdAsync(id);
+            var mentorDto = mapper.Map<Mentor, MentorDto>(mentor);
+            return mentorDto;
+        }
+    }
+}
---
+++ ServiceAbstraction/IMentorService.cs
@@ -0,0 +1,15 @@
+﻿using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+    public interface IMentorService
+    {
+        Task<IEnumerable<MentorDto>> GetAllMentorsAsync();
+        Task<MentorDto> GetMentorById(int id);
+    }
+}
